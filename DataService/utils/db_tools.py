e1eeda3b627372fd8eaa9e200e12b8096f1bb10c
AllenShen <932142511@qq.com>
2018-05-20 15:44:01 +0800

    系统表初始化工具

---
+++ DataService/utils/db_tools.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('../')
+from DataService.models import get_session, Tables, Fields, ModuleReg
+
+def init_sys_tables():
+    """
+    初始化系统表Tables, Fields, Sensitivity
+    :return:
+    """
+    session = get_session()
+    r = session.execute("select TABLE_NAME from information_schema.TABLES where table_schema = 'ims'").fetchall()
+    # print(r[0][0])
+    added = True
+    if not added:
+        for i in range(len(r)):
+            table_name = r[i][0]
+            new_t = Tables(name=table_name, sensitivity=0)
+            session.add(new_t)
+            session.commit()
+            f = session.execute("SELECT COLUMN_NAME FROM information_schema.columns\
+                                  WHERE table_schema = 'ims' AND table_name = 'module_reg'").fetchall()
+            for j in range(len(f)):
+                new_f = Fields(name=f[j][0], table_id=new_t.id, sensitivity=0)
+                session.add(new_f)
+                session.commit()
+
+
+if __name__ == '__main__':
+    init_sys_tables()
