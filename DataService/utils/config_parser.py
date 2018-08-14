8b8812aa618e8533770276e9c8a768cabb9e3812
AllenShen <932142511@qq.com>
2018-08-14 15:45:17 +0800

    add: 配置文件自动更新

--- DataService/utils/config_parser.py
+++ DataService/utils/config_parser.py
@@ -1,14 +1,31 @@
 import sys
 import os
 import json
+import copy
+import operator
+
+
+def _auto_update(func):
+    """
+    自动更新装饰器
+    """
+    def wrapper(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        if self.auto_update \
+                and not operator.eq(self._config_bak, self.config):
+            self.update()
+            self._config_bak = copy.deepcopy(self.config)
+
+    return wrapper
 
 class Config:
-    def __init__(self, config_path='conf.json'):
+    def __init__(self, config_path='conf.json', auto_update=True):
         self.config_path = self._relative_to_abs(config_path)
+        self.auto_update = auto_update
         # print(path)
         with open(self.config_path, 'r') as f:
             self.config = json.load(f) # 此处不处理IOError
-
+            self._config_bak = copy.deepcopy(self.config) # 做深拷贝作为修改快照
 
     @staticmethod
     def _relative_to_abs(path):
@@ -19,12 +36,13 @@ class Config:
         path = file_dir + path
         return path
 
-    def __getitem__(self, item):
-        return self.config[item]
-
-    def __setitem__(self, key, value):
-        self.config[key] = value
-
     def update(self):
         with open(self.config_path, 'w') as f:
             json.dump(self.config, f, indent=4)
+
+    @_auto_update
+    def __setitem__(self, key, value):
+        self.config[key] = value
+
+    def __getitem__(self, item):
+        return self.config[item]
