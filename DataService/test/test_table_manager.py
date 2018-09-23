26873c0cd5b44017201fd124a4699bdbfa074949
AllenShen <932142511@qq.com>
2018-09-23 14:39:44 +0800

    add: prefix构造app

--- DataService/test/test_table_manager.py
+++ DataService/test/test_table_manager.py
@@ -4,6 +4,7 @@ from ..table_manager import table_manager
 from ..models import get_session
 from ..models.sys import *
 from ..utils.db_tools import all_tables
+from ..manager import create_app
 
 table_info1 = [
                 {
@@ -51,54 +52,61 @@ test_info = [table_info1]
 
 @pytest.fixture(scope='module')
 def table_test_fixture():
+    app = create_app('test')
+    app_ctx = app.app_context()
+    app_ctx.push()
     mod = module_manager(id=1)
-    t_manager = table_manager(mod)
-    yield t_manager
+    manager = table_manager(mod)
+    version = manager.session.execute('select * from alembic_version').first()[0]
+    print(version)
+    manager.set_test_rollback_version(version)
+    yield manager
     for i in test_info:
-        t_manager._test_teardown(i)
+        manager._test_teardown(i)
 
 
-
-
-@pytest.mark.usefixtures("table_test_fixture")
 @pytest.mark.parametrize('table_info', [
     table_info1,
 ])
-def test_register_table(table_info):
+def test_register_table(table_test_fixture, table_info):
     # 测试要求结果：
     # Table Field表更新
     # 新表生成(新表生成说明数据库已经正常迁移，映射文件正常)
     # *api生成 等待api管理器开发完成
-    session = get_session()
-    t_manager.register_table(table_info)
+    manager = table_test_fixture
+    session = manager.session
+    manager.register_table(table_info)
 
     all_table = all_tables()
     # Table Field表更新
     for table in table_info:
-        table_row = session.query(Tables).filter_by(name=table['table_name'], owner_id=mod.get('id')).first()
+        table_row = session.query(Tables).filter_by(name=table['table_name'], owner_id=manager.current_mod.get('id')).first()
         assert table_row is not None
         # 新表生成
+        print(table_row)
         assert table['table_name'] in all_table
         for field in table['fields']:
             field_row = session.query(Fields).filter_by(name=field, table_id=table_row.id).first()
             assert field_row is not None
 
-@pytest.mark.usefixtures("table_test_fixture")
+
 @pytest.mark.parametrize('table_info', [
     table_info1,
 ])
-def test_delete_table(table_info):
+def test_delete_table(table_test_fixture, table_info):
     # 测试结果要求：
     # Table Field表更新
     # 旧表删除
     session = get_session()
+    manager = table_test_fixture
+
     for table in table_info:
-        t_manager.delete_table(table['table_name'])
+        manager.delete_table(table['table_name'])
 
     all_table = all_tables()
     # Table Field表更新
     for table in table_info:
-        table_row = session.query(Tables).filter_by(name=table['table_name'], owner_id=mod.get('id')).first()
+        table_row = session.query(Tables).filter_by(name=table['table_name'], owner_id=manager.current_mod.get('id')).first()
         assert table_row is None
         # 新表生成
         assert table['table_name'] not in all_table
